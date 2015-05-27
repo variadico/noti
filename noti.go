2b3cfc40181dbe54f7bef72c6c3cd47b8f1fdc44
variadico <variadico@users.noreply.github.com>
2015-05-27 11:36:20 -0700

    add long hand flags; display more usage help

--- noti.go
+++ noti.go
@@ -9,40 +9,86 @@ import (
 )
 
 var (
-	title = flag.String("t", "Utility Name", "Title of notification.")
-	mesg  = flag.String("m", "Done!", "Message notification will display.")
-	sound = flag.String("s", "Ping", "Sound to play when notified.")
+	title   = flag.String("t", "Utility Name", "")
+	mesg    = flag.String("m", "Done!", "")
+	sound   = flag.String("s", "Ping", "")
+	version = flag.Bool("v", false, "")
+	help    bool
 )
 
 func init() {
+	flag.StringVar(title, "title", "Utility Name", "")
+	flag.StringVar(mesg, "message", "Done!", "")
+	flag.StringVar(sound, "sound", "Ping", "")
+	flag.BoolVar(version, "version", false, "")
+	flag.BoolVar(&help, "help", false, "")
+
 	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, `Usage: noti [-tms] [utility [args...]]
+		fmt.Fprintln(os.Stderr, `NOTI
+    display a notification in os x after a terminal process finishes.
 
-    -t    Title of notification. If no arguments passed, default is "noti",
-          else default is utility name.
+USAGE
+    noti [options] [utility [args...]]
 
-    -m    Message notification will display. Default is "Done!"
+OPTIONS
+    -t, -title
+        Set the notification title. If no arguments passed, default is "noti",
+        else default is utility name.
 
-    -s    Sound to play when notified. Default is Ping. Possible options
-          are Basso, Blow, Bottle, Frog, Funk, Glass, Hero, Morse, Ping,
-          Pop, Purr, Sosumi, Submarine, Tink. Check /System/Library/Sounds
-          for available sounds.
+    -m, -message
+        Set notification message. Default is "Done!"
 
-    -h    Display usage information and exit.`)
+    -s, -sound
+        Set notification sound. Default is Ping. Possible options are Basso,
+        Blow, Bottle, Frog, Funk, Glass, Hero, Morse, Ping, Pop, Purr, Sosumi,
+        Submarine, Tink. Check /System/Library/Sounds for available sounds.
+
+    -v, -version
+        Print noti version and exit.
+
+    -h, -help
+        Display usage information and exit.
+
+EXAMPLES
+    Display a notification when tar finishes compressing files.
+        noti tar -cjf music.tar.bz2 Music/
+
+    Display a notification when apt-get finishes updating on a remote server.
+        noti ssh you@server.com apt-get update
+
+    Set the notification title to "homebrew" and message to "Up to date" and
+    display it after Homebrew finishes updating.
+        noti -t "homebrew" -m "up to date" brew update
+
+    You can also add noti after a command, in case you forgot at the beginning.
+        clang foo.c -Wall -lm -L/usr/X11R6/lib -lX11 -o bizz; noti
+`)
 	}
 }
 
 func main() {
 	flag.Parse()
 
+	if help {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *version {
+		fmt.Println("noti version 1.0.0")
+		return
+	}
+
 	if len(flag.Args()) == 0 {
 		if *title == "Utility Name" {
 			*title = "noti"
 		}
+
 		if err := notify(*title, *mesg, *sound); err != nil {
 			log.Println(err)
 			os.Exit(1)
 		}
+
 		return
 	}
 
