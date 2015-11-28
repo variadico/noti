ee07a6d8a1cf0ef0bb1016b8e9ef97d935f2ac71
variadico <variadico@users.noreply.github.com>
2015-11-27 22:24:33 -0800

    Break up notifier interface

--- notifier.go
+++ notifier.go
@@ -1,17 +1,20 @@
-// Package noti describes a basic notification.
 package noti
 
-// Notifier describes a basic notification and can trigger a notification. At
-// minimum, a Notifier should have a notification title and message.
 type Notifier interface {
+	// Notify triggers a notification.
+	Notify() error
+}
+
+type Titler interface {
 	// GetTitle returns a notification's title.
 	GetTitle() string
 	// SetTitle sets a notification's title.
 	SetTitle(string)
+}
+
+type Messager interface {
 	// GetMessage returns a notification's message.
 	GetMessage() string
 	// SetMessage sets a notification's message.
 	SetMessage(string)
-	// Notify triggers a notification.
-	Notify() error
 }
