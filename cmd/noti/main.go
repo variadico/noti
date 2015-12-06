5c4b7036c1ce40ff259cab38f3f6f9e2c5a11125
variadico <variadico@users.noreply.github.com>
2015-12-06 02:55:43 -0800

    Add flag to save previous flag state.

--- cmd/noti/main.go
+++ cmd/noti/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 
 	"github.com/variadico/noti/pushbullet"
 )
@@ -23,6 +27,8 @@ OPTIONS
     -p, -pushbullet
         Send a Pushbullet notification. Access token must be set in
         PUSHBULLET_ACCESS_TOKEN environment variable.
+    -s, -save
+        Save current flag set.
     -v, -version
         Print noti version and exit.
     -h, -help
@@ -35,6 +41,10 @@ EXAMPLES
     Create a reminder to get back to a friend.
         noti -t "Reply to Pedro" gsleep 5m &
 `
+
+const (
+	flagState = ".noticonfig.json"
+)
 
 var usageText string
 
@@ -49,8 +59,26 @@ func main() {
 	flag.BoolVar(version, "version", false, "")
 	help := flag.Bool("h", false, "")
 	flag.BoolVar(help, "help", false, "")
+	save := flag.Bool("S", false, "")
+	flag.BoolVar(save, "save", false, "")
 	flag.Usage = func() { log.Println(usageText) }
 	flag.Parse()
+
+	if *save {
+		if err := saveFlags(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		usr, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := os.Stat(filepath.Join(usr.HomeDir, flagState)); err == nil {
+			if err := loadFlags(); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
 
 	if *help {
 		fmt.Println(usageText)
@@ -73,6 +101,59 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+}
+
+func saveFlags() error {
+	config := make(map[string]string)
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Name == "save" || f.Name == "S" {
+			return
+		}
+		config[f.Name] = f.Value.String()
+	})
+
+	bs, err := json.MarshalIndent(config, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filepath.Join(usr.HomeDir, flagState), bs, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func loadFlags() error {
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	bs, err := ioutil.ReadFile(filepath.Join(usr.HomeDir, flagState))
+	if err != nil {
+		return err
+	}
+
+	config := make(map[string]string)
+	err = json.Unmarshal(bs, &config)
+	if err != nil {
+		return err
+	}
+
+	flag.VisitAll(func(f *flag.Flag) {
+		if v, exists := config[f.Name]; exists {
+			f.Value.Set(v)
+		}
+	})
+
+	return nil
 }
 
 // processArgs executes any utility args. In addition, it also sets the
