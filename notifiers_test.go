640c16b1407ee368491a346933f9696935c326c2
variadico <variadico@users.noreply.github.com>
2016-02-24 20:48:06 -0800

    Add unit tests

---
+++ notifiers_test.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestPushbulletNotify(t *testing.T) {
+	orig := struct {
+		url string
+		tok string
+	}{
+		url: pushbulletAPI,
+		tok: os.Getenv(pushbulletTokEnv),
+	}
+	defer func() {
+		pushbulletAPI = orig.url
+		os.Setenv(pushbulletTokEnv, orig.tok)
+	}()
+
+	os.Unsetenv(pushbulletTokEnv)
+	if err := pushbulletNotify(); err == nil {
+		t.Error("Missing access token.")
+	}
+
+	os.Setenv(pushbulletTokEnv, "fu")
+	var hitServer bool
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		hitServer = true
+
+		if r.Method != "POST" {
+			t.Error("HTTP method should be POST")
+		}
+
+		if r.Header.Get("Access-Token") == "" {
+			t.Error("Missing access token.")
+		}
+		if r.Header.Get("Content-Type") != "application/json" {
+			t.Error("Content type should be application/json.")
+		}
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if string(b) == "" {
+			t.Error("Missing payload.")
+		}
+	}))
+	defer ts.Close()
+
+	pushbulletAPI = ts.URL
+	if err := pushbulletNotify(); err != nil {
+		t.Error(err)
+	}
+
+	if !hitServer {
+		t.Error("Didn't reach server.")
+	}
+}
+
+func TestSlackNotify(t *testing.T) {
+	orig := struct {
+		url  string
+		tok  string
+		dest string
+	}{
+		url:  slackAPI,
+		tok:  os.Getenv(slackTokEnv),
+		dest: os.Getenv(slackDestEnv),
+	}
+	defer func() {
+		slackAPI = orig.url
+		os.Setenv(slackTokEnv, orig.tok)
+		os.Setenv(slackDestEnv, orig.dest)
+	}()
+
+	os.Unsetenv(slackTokEnv)
+	if err := slackNotify(); err == nil {
+		t.Error("Missing access token.")
+	}
+
+	os.Unsetenv(slackDestEnv)
+	if err := slackNotify(); err == nil {
+		t.Error("Missing destination.")
+	}
+
+	os.Setenv(slackTokEnv, "fu")
+	os.Setenv(slackDestEnv, "fa")
+
+	var hitServer bool
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		hitServer = true
+
+		if r.Method != "POST" {
+			t.Error("HTTP method should be POST")
+		}
+
+		if r.FormValue("token") == "" {
+			t.Error("Missing access token.")
+		}
+		if r.FormValue("channel") == "" {
+			t.Error("Missing destination.")
+		}
+	}))
+	defer ts.Close()
+
+	slackAPI = ts.URL
+	if err := slackNotify(); err != nil {
+		t.Error(err)
+	}
+
+	if !hitServer {
+		t.Error("Didn't reach server.")
+	}
+}
+
+func TestHipChatNotify(t *testing.T) {
+	orig := struct {
+		url  string
+		tok  string
+		dest string
+	}{
+		url:  hipChatAPI,
+		tok:  os.Getenv(hipChatTokEnv),
+		dest: os.Getenv(hipChatDestEnv),
+	}
+	defer func() {
+		hipChatAPI = orig.url
+		os.Setenv(hipChatTokEnv, orig.tok)
+		os.Setenv(hipChatDestEnv, orig.dest)
+	}()
+
+	os.Unsetenv(hipChatTokEnv)
+	if err := hipChatNotify(); err == nil {
+		t.Error("Missing access token.")
+	}
+
+	os.Unsetenv(hipChatDestEnv)
+	if err := hipChatNotify(); err == nil {
+		t.Error("Missing destination.")
+	}
+
+	os.Setenv(hipChatTokEnv, "fu")
+
+	var hitServer bool
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		hitServer = true
+
+		if r.Method != "POST" {
+			t.Error("HTTP method should be POST")
+		}
+
+		if r.Header.Get("Authorization") == "" {
+			t.Error("Missing access token.")
+		}
+		if r.Header.Get("Content-Type") != "application/json" {
+			t.Error("Content type should be application/json.")
+		}
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if string(b) == "" {
+			t.Error("Missing payload.")
+		}
+	}))
+	defer ts.Close()
+
+	// In real life, to calculate hipChatAPI, we need to Sprintf the
+	// destination env var into the URL. This just pretends that HipChat Room
+	// is at ts.URL.
+	os.Setenv(hipChatDestEnv, ts.URL)
+	hipChatAPI = "%s"
+	if err := hipChatNotify(); err != nil {
+		t.Error(err)
+	}
+
+	if !hitServer {
+		t.Error("Didn't reach server.")
+	}
+}
+
+func TestPushoverNotify(t *testing.T) {
+	orig := struct {
+		url  string
+		tok  string
+		dest string
+	}{
+		url:  pushoverAPI,
+		tok:  os.Getenv(pushoverTokEnv),
+		dest: os.Getenv(pushoverDestEnv),
+	}
+	defer func() {
+		slackAPI = orig.url
+		os.Setenv(pushoverTokEnv, orig.tok)
+		os.Setenv(pushoverDestEnv, orig.dest)
+	}()
+
+	os.Unsetenv(pushoverTokEnv)
+	if err := pushoverNotify(); err == nil {
+		t.Error("Missing access token.")
+	}
+
+	os.Unsetenv(pushoverDestEnv)
+	if err := pushoverNotify(); err == nil {
+		t.Error("Missing destination.")
+	}
+
+	os.Setenv(pushoverTokEnv, "fu")
+	os.Setenv(pushoverDestEnv, "fa")
+
+	var hitServer bool
+	ts := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		hitServer = true
+
+		if r.Method != "POST" {
+			t.Error("HTTP method should be POST")
+		}
+
+		if r.FormValue("token") == "" {
+			t.Error("Missing access token.")
+		}
+		if r.FormValue("user") == "" {
+			t.Error("Missing destination.")
+		}
+	}))
+	defer ts.Close()
+
+	pushoverAPI = ts.URL
+	if err := pushoverNotify(); err != nil {
+		t.Error(err)
+	}
+
+	if !hitServer {
+		t.Error("Didn't reach server.")
+	}
+}
