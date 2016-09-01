4fc5a8fcf375d80bd5e844d162c30d3901e15a64
Adam Baxter <voltagex@voltagex.org>
2016-09-01 20:03:52 +1000

    Fix formatting of banner_windows

--- banner/banner_windows.go
+++ banner/banner_windows.go
@@ -3,25 +3,23 @@
 package banner
 
 import (
-	"github.com/variadico/noti"
 	toast "github.com/jacobmarshall/go-toast"
-    "log"
+	"github.com/variadico/noti"
+	"log"
 )
 
 // Notify displays a Windows 10 Toast Notification
-func Notify(n noti.Params) error {
-	 notification := toast.Notification{
-        AppID: "noti",
-        Title: n.Title,
-        Message: n.Message,
-        Icon: "",
-        Actions: nil}
+func Notify(n noti.Params) {
+	notification := toast.Notification{
+		AppID:   "noti",
+		Title:   n.Title,
+		Message: n.Message,
+		Icon:    "",
+		Actions: nil}
 
-    err := notification.Push()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    if err != nil {
-        log.Fatalln(err)
-    }
-    
-	return nil
+	return notification.Push()
 }
