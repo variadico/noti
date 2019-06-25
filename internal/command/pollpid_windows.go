8eb3c40f47d6d7b572d4bd65f5620c8f7e8e7551
Jonathan Gilbert <JonathanG@iQmetrix.com>
2019-06-25 00:09:39 -0500

    Implement pollPID for Windows and BSD kernels (#93)

--- internal/command/pollpid_windows.go
+++ internal/command/pollpid_windows.go
@@ -1,10 +1,36 @@
+// +build windows
+
 package command
 
 import (
 	"errors"
 	"time"
+	"strconv"
+	"syscall"
 )
 
-func pollPID(_ int, _ time.Duration) error {
-	return errors.New("pwatch not supported on this platform")
+var (
+	kernel32 = syscall.NewLazyDLL("kernel32")
+
+	openProcess = kernel32.NewProc("OpenProcess")
+	waitForSingleObject = kernel32.NewProc("WaitForSingleObject")
+)
+
+const _SYNCHRONIZE = 0x00100000
+const _INFINITE = 0xFFFFFFFF
+
+func pollPID(pid int, _ time.Duration) error {
+	hProcess, _, lastError := openProcess.Call(_SYNCHRONIZE, 0, uintptr(pid))
+
+	if hProcess == 0 {
+		return errors.New("OpenProcess failed with " + lastError.Error())
+	}
+
+	result, _, lastError := waitForSingleObject.Call(hProcess, _INFINITE)
+
+	if result != 0 {
+		return errors.New("WaitForSingleObject failed with " + strconv.Itoa(int(result)) + ", last error " + lastError.Error())
+	}
+
+	return nil
 }
