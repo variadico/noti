7636e1dcf7cc4c9f48dd314a5cffab487fc3b9bb
Jaime Piña <jaime@variadi.co>
2019-06-24 23:16:00 -0700

    Refactor windows poll

--- internal/command/pollpid_windows.go
+++ internal/command/pollpid_windows.go
@@ -3,33 +3,29 @@
 package command
 
 import (
-	"errors"
-	"time"
-	"strconv"
+	"fmt"
 	"syscall"
+	"time"
 )
-
-var (
-	kernel32 = syscall.NewLazyDLL("kernel32")
-
-	openProcess = kernel32.NewProc("OpenProcess")
-	waitForSingleObject = kernel32.NewProc("WaitForSingleObject")
-)
-
-const _SYNCHRONIZE = 0x00100000
-const _INFINITE = 0xFFFFFFFF
 
 func pollPID(pid int, _ time.Duration) error {
-	hProcess, _, lastError := openProcess.Call(_SYNCHRONIZE, 0, uintptr(pid))
+	kernel32 := syscall.NewLazyDLL("kernel32")
+	openProcess := kernel32.NewProc("OpenProcess")
+	waitForSingleObject := kernel32.NewProc("WaitForSingleObject")
 
+	const (
+		synchronize = 0x00100000
+		infinite    = 0xFFFFFFFF
+	)
+
+	hProcess, _, lastErr := openProcess.Call(synchronize, 0, uintptr(pid))
 	if hProcess == 0 {
-		return errors.New("OpenProcess failed with " + lastError.Error())
+		return fmt.Errorf("proc OpenProcess failed: %v", lastErr)
 	}
 
-	result, _, lastError := waitForSingleObject.Call(hProcess, _INFINITE)
-
+	result, _, lastErr := waitForSingleObject.Call(hProcess, infinite)
 	if result != 0 {
-		return errors.New("WaitForSingleObject failed with " + strconv.Itoa(int(result)) + ", last error " + lastError.Error())
+		return fmt.Errorf("proc WaitForSingleObject failed: %d %v", result, lastErr)
 	}
 
 	return nil
